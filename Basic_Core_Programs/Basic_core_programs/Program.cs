ddc5e0ed6afd00faf5b24cef482b038fe9826751
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-12 00:59:36 +0530

    [Prasoon] added Flip Coin percentage

---
+++ Basic_Core_Programs/Basic_core_programs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Basic_core_programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter 1 for Heads and Tails");
+            int ans = Convert.ToInt32(Console.ReadLine());
+            switch (ans)
+            {
+                case 1: 
+                    FlipCoin Fc = new FlipCoin();
+                    Fc.HeadsOrTailPercentage();
+                    break;
+                default:
+                    Console.WriteLine("Entered value is incorrect");
+                    break;
+            }
+        }
+    }
+}
