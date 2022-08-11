1dc68a0235801088be3d111f13ca733c7cc3585c
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-12 01:14:29 +0530

    [Prasoon] Added Leap Year program

--- Basic_Core_Programs/Basic_core_programs/Program.cs
+++ Basic_Core_Programs/Basic_core_programs/Program.cs
@@ -6,12 +6,17 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Enter 1 for Heads and Tails");
+            Console.WriteLine("Enter 2 for Leap Year");
             int ans = Convert.ToInt32(Console.ReadLine());
             switch (ans)
             {
                 case 1: 
                     FlipCoin Fc = new FlipCoin();
                     Fc.HeadsOrTailPercentage();
+                    break;
+                case 2:
+                    LeapYear Ly = new LeapYear();
+                    Ly.leapYear();
                     break;
                 default:
                     Console.WriteLine("Entered value is incorrect");
