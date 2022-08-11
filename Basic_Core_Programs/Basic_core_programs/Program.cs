69f9d3fc3ba683342d4f687d7f37c6615927552f
Prasoon-mishra <prasoonmishra987@gmail.com>
2022-08-12 01:28:31 +0530

    [Prasoon] Added power of Two program

--- Basic_Core_Programs/Basic_core_programs/Program.cs
+++ Basic_Core_Programs/Basic_core_programs/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Enter 1 for Heads and Tails");
             Console.WriteLine("Enter 2 for Leap Year");
+            Console.WriteLine("Enter 3 for Power of Two");
             int ans = Convert.ToInt32(Console.ReadLine());
             switch (ans)
             {
@@ -17,6 +18,10 @@
                 case 2:
                     LeapYear Ly = new LeapYear();
                     Ly.leapYear();
+                    break;
+                case 3:
+                    PowerOFTwo Pf =new PowerOFTwo();
+                    Pf.powOfTwo();
                     break;
                 default:
                     Console.WriteLine("Entered value is incorrect");
